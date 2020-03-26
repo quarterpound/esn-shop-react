26a22a84fab26f23a684de0dc67d6892acae0fa7
Ali Gasimzade <gasimzadeali@gmail.com>
2020-03-26 04:32:34 +0400

    Switch from http from https

--- src/c.js
+++ src/c.js
@@ -1,4 +1,4 @@
-const SERVER = "http://esn.az:3443";
+const SERVER = "https://esn.az:3443";
 const IMAGES = `${SERVER}/images`;
 const ITEMS = `${SERVER}/items`;
 const PURCHASES = `${SERVER}/purchases`;
